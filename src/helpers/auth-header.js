50809ddc8a78dc11d2259d299ffd93136e89ba06
unknown <albertbenig@gmail.com>
2022-04-28 18:11:04 +0200

    make it better

--- src/helpers/auth-header.js
+++ src/helpers/auth-header.js
@@ -2,6 +2,10 @@ export function authHeader () {
   const user = JSON.parse(localStorage.getItem('user'))
   if (user && user.token) {
     return {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      'Access-Control-Allow-Credentials': true,
       Authorization: `JWT ${user.token}`,
       'Content-Type': 'application/json'
     }
